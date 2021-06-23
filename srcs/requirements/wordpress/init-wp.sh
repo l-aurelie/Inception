0fd0b9a53c203326703383398ac7e85b2665c522
Aurelie Leconte <aurelieleconte.al@gmail.com>
2021-06-23 18:03:27 +0200

    wp-cli install

--- srcs/requirements/wordpress/init-wp.sh
+++ srcs/requirements/wordpress/init-wp.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-mv wordpress /var/www/
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
+
+wp core download --path=/var/www --locale=fr_FR
+
+#mv wordpress /var/www/
 exec "$@"
