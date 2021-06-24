80a59a0af034318b4f2534ecb87cf9487d73d936
Aurelie Leconte <aurelieleconte.al@gmail.com>
2021-06-24 14:57:11 +0200

    config wp done

--- srcs/requirements/wordpress/init-wp.sh
+++ srcs/requirements/wordpress/init-wp.sh
@@ -5,5 +5,10 @@ chmod +x /usr/local/bin/wp
 
 wp core download --path=/var/www --locale=fr_FR
 
-#mv wordpress /var/www/
+wp config create --dbname=wp_db --dbuser=username --dbpass=userpass --dbhost=mariadb --path=/var/www/ --dbprefix=fyw43WP
+
+wp core install --url="localhost" --title="Beau site" --admin_user="chief" --admin_password="chiefpass" --admin_email="chief@email.com" --path=/var/www/
+
+wp user create membername member@email.com --role="author" --user_pass="memberpass" --path="/var/www"
+
 exec "$@"
