149a7473b8956f8026a3e1cf590011f75c62740e
Aurelie Leconte <aurelieleconte.al@gmail.com>
2021-06-25 17:41:08 +0200

    nom domain, 443

--- srcs/requirements/wordpress/init-wp.sh
+++ srcs/requirements/wordpress/init-wp.sh
@@ -5,10 +5,10 @@ chmod +x /usr/local/bin/wp
 
 wp core download --path=/var/www --locale=fr_FR
 
-wp config create --dbname=wp_db --dbuser=username --dbpass=userpass --dbhost=mariadb --path=/var/www/ --dbprefix=fyw43WP
+wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost=mariadb --path=/var/www/ --dbprefix=wp_
 
-wp core install --url="localhost" --title="Beau site" --admin_user="chief" --admin_password="chiefpass" --admin_email="chief@email.com" --path=/var/www/
+wp core install --url="aleconte.42.fr" --title="Beau site" --admin_user="$WP_ADMIN" --admin_password="$WP_ADPASS" --admin_email="$WP_ADMIN@email.com" --path=/var/www/
 
-wp user create membername member@email.com --role="author" --user_pass="memberpass" --path="/var/www"
+wp user create $WP_USER $WP_USER@email.com --role="author" --user_pass="$WP_USPASS" --path="/var/www"
 
 exec "$@"
